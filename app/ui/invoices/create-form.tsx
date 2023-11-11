51e3b877a2311f002e36c62289653296b98a3c21
Kenny Elshoff <github@kennyelshoff.com>
2023-11-11 08:45:51 -0500

    chapter 12

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,7 +8,7 @@ import {
   CurrencyDollarIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
-import { Button } from "../button";
+import { Button } from "@/app/ui/button";
 import { createInvoice } from "@/app/lib/actions";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
