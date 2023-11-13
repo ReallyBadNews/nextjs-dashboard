9d19611af17e9eed0eecb5b726cd2a6438c1330f
Kenny Elshoff <github@kennyelshoff.com>
2023-11-12 22:46:12 -0500

    add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,36 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import Search from "@/app/ui/search";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  return (
+    <main>
+      <div className="w-full">
+        <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+          Customers
+        </h1>
+        <Search placeholder="Search customers..." />
+        <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+          <CustomersTable query={query} />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
