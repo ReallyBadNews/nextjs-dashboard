38b6877580942c7e60ca0e72ab36dbb4ff24f987
Kenny Elshoff <github@kennyelshoff.com>
2023-12-07 22:10:39 -0500

    fix: remove artificial delay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,6 @@ export async function fetchRevenue() {
   noStore();
 
   try {
-    // Artificially delay a reponse for demo purposes.
-    // Don't do this in real life :)
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log("Data fetch complete after 3 seconds.");
